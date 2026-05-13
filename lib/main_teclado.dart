a911fdac198e8757a2ab7335287b958261f8e6f7
vitoriapmello-lang <vitoria.p.mello@aluno.senai.br>
2026-05-13 11:11:28 -0300

    tecladinhos

---
+++ lib/main_teclado.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaTeclados(),
+    );
+  }
+}
+
+class TelaTeclados extends StatelessWidget {
+  final nomeController = TextEditingController();
+  final emailController = TextEditingController();
+  final idadeController = TextEditingController();
+  final telefoneController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Exemplo de Teclados"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+
+            TextField(
+              controller: nomeController,
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                labelText: "Nome",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              controller: idadeController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Idade",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            SizedBox(height: 15),
+
+            TextField(
+              controller: telefoneController,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: "Telefone",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
